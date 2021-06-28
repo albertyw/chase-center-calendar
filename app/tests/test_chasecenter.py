293a87f9a5bf98870ebd2c078d9554e114a78592
Albert Wang <git@albertyw.com>
2021-06-27 17:55:53 -0700

    Add test

--- app/tests/test_chasecenter.py
+++ app/tests/test_chasecenter.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from unittest import TestCase
 
 from app import chasecenter
@@ -10,3 +11,17 @@ class TestGetRawEvents(TestCase):
         self.assertIn('contentByType', events['data'])
         self.assertIn('items', events['data']['contentByType'])
         self.assertTrue(len(events['data']['contentByType']['items']) > 0)
+
+
+class TestGetEvents(TestCase):
+    def test_get_events(self) -> None:
+        events = chasecenter.get_events()
+        self.assertTrue(len(events) > 0)
+        event = events[0]
+        self.assertTrue(isinstance(event.title, str))
+        self.assertTrue(isinstance(event.date_string, str))
+        self.assertTrue(isinstance(event.date, datetime))
+        self.assertTrue(isinstance(event.ticket_required, bool))
+        self.assertTrue(isinstance(event.ticket_available, bool))
+        self.assertTrue(isinstance(event.ticket_sold_out, bool))
+        self.assertTrue(isinstance(event.duration, int))
