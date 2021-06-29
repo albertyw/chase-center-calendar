59076a78a60baa2bf0d73a8199a85e42c02180f5
Albert Wang <git@albertyw.com>
2021-06-28 23:23:06 -0700

    Add a page to display ical data

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from typing import Any
 from flask import Blueprint, render_template
 from varsnap import varsnap
 
-from app import chasecenter
+from app import chasecenter, ical
 
 handlers = Blueprint('handlers', __name__)
 
@@ -13,6 +13,13 @@ def index() -> Any:
     events = chasecenter.get_events()
     events = [e for e in events if e.show]
     return render_template("index.htm", events=events)
+
+
+@handlers.route("/ical_view")
+def ical_view() -> Any:
+    events = chasecenter.get_events()
+    cal = ical.generate_calendar(events)
+    return render_template("ical_view.htm", cal=cal)
 
 
 @handlers.route("/about")
