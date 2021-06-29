f3a801ade3ec4c292a656d2d808e6a43b95602c7
Albert Wang <git@albertyw.com>
2021-06-28 22:28:55 -0700

    Add date_string function

--- app/ical.py
+++ app/ical.py
@@ -1,6 +1,8 @@
+import datetime
 from typing import List
 
 from icalendar import Calendar
+import pytz
 
 from app import chasecenter
 
@@ -9,3 +11,12 @@ def generate_calendar(events: List[chasecenter.Event]) -> str:
     cal = Calendar()
     cal['summary'] = 'Chase Center Events'
     return str(cal.to_ical().decode('utf-8'))
+
+
+def date_string(dt: datetime.datetime) -> str:
+    utc = dt.astimezone(pytz.utc).replace(tzinfo=None)
+    formatted = utc.isoformat()
+    formatted = formatted.replace('-', '')
+    formatted = formatted.replace(':', '')
+    formatted += 'Z'
+    return formatted
