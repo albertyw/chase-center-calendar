1397fa2cd41bd6e33d8918208e2064238dbc5640
Albert Wang <git@albertyw.com>
2021-10-10 16:50:31 -0700

    Parse oracle park into Events

--- app/oraclepark.py
+++ app/oraclepark.py
@@ -1,23 +1,53 @@
-from typing import Any, List
+import datetime
+from typing import cast, List, Optional
 
+from bs4 import BeautifulSoup
+from dateutil import parser as dateutilparser
 import requests
 
-from app.event import Event
+from app.event import Event, TIMEZONE
 
 
 URL = "https://dothebay.com/venues/oracle-park/events"
 
 
-def get_raw_events() -> Any:
+def get_raw_events() -> List[BeautifulSoup]:
     response = requests.get(URL)
-    print(response.content)
-    return response.content
+    soup = BeautifulSoup(response.content, 'html.parser')
+    event_divs = soup.find_all('div', class_='ds-events-group')
+    return event_divs
+
+
+def parse_event_div(event_div: BeautifulSoup) -> Event:
+    event = Event()
+    event.id = cast(Optional[str], event_div.find_all('a', class_='ds-btn-ical')[0]['data-ds-id'])
+    event.title = cast(str, event_div.find_all('span', attrs={'itemprop': 'name'})[0].get_text())
+    event.slug = cast(Optional[str], event.title)
+    event.subtitle = cast(Optional[str], event.title)
+    event.date_string = cast(str, event_div.find_all('meta', attrs={'itemprop': 'startDate'})[0]['datetime'])
+    date = dateutilparser.isoparse(event.date_string)
+    event.date = date.astimezone(TIMEZONE)
+    event.date_string = event.date.isoformat()
+    location = event_div.find_all('div', attrs={'itemprop': 'location'})[0]
+    event.location_name = cast(Optional[str], location.find_all('span', attrs={'itemprop': 'name'})[0].get_text())
+    event.location_type = ''
+    event.ticket_required = True
+    event.ticket_available = True
+    event.ticket_sold_out = False
+    event.hide_road_game = False
+    event.duration = 4
+    return event
 
 
 def get_events() -> List[Event]:
-    get_raw_events()
-    return []
+    event_divs = get_raw_events()
+    events: List[Event] = []
+    for event_div in event_divs:
+        event = parse_event_div(event_div)
+        events.append(event)
+    print(events[0].__dict__)
+    return events
 
 
 if __name__ == '__main__':
-    get_raw_events()
+    get_events()
