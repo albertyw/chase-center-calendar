716bdb37ff262df3ab030ec38339fad28bf7ca0f
Albert Wang <git@albertyw.com>
2022-04-08 23:55:25 -0400

    Fix mock error

--- app/tests/test_chasecenter.py
+++ app/tests/test_chasecenter.py
@@ -80,13 +80,13 @@ class TestGetRawEvents(TestCase):
 
     @patch('requests.post')
     def test_get_events_no_json(self, mock_post: MagicMock) -> None:
-        mock_post.json.side_effect = json.JSONDecodeError
+        mock_post().json.side_effect = json.JSONDecodeError("", "", 0)
         events = chasecenter.get_raw_events()
         self.assertEqual(len(events), 0)
 
     @patch('requests.post')
     def test_get_events_corrupt_json(self, mock_post: MagicMock) -> None:
-        mock_post.json.return_value = {'asdf': 'qwer'}
+        mock_post().json.return_value = {'asdf': 'qwer'}
         events = chasecenter.get_raw_events()
         self.assertEqual(len(events), 0)
 
